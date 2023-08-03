38514ce6288d59237645273e934e9fa75e576ba7
Valerie Yauw <valerie.yauw@gmail.com>
2023-08-03 17:58:07 +0700

    rev

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Controller = require("../controllers/controller");
 const SessionController = require("../controllers/session");
 
-router.get("/", Controller.showLandingPage);
+router.get('/', Controller.showLandingPage);
 
 router.get("/session-data", Controller.showSessionData);
 
